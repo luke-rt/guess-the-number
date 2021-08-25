edce5ad09ab199419a2c046ff6885e7dfeb5ddb5
Luke T <56455367+ltgr@users.noreply.github.com>
2021-08-25 17:21:42 -0400

    Update darwin.mk

--- build/darwin.mk
+++ build/darwin.mk
@@ -1,6 +1,4 @@
-LD=i386-elf-ld
-
-CC=/opt/homebrew/bin/gcc-11 -elf_i386 -m32
+CC=/opt/homebrew/bin/gcc-11 -elf_i386
 LD=/usr/bin/ld -arch i386
 AS=/opt/homebrew/bin/nasm
 EMU=/opt/homebrew/bin/qemu-system-i386
